dc87d54889294a1ebf67caadf8045cbd3d754cf3
Ben Toews <mastahyeti@gmail.com>
2018-09-17 10:39:28 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,35 @@
+ # tweetsodium
+
+ This library implements [libsodium's sealed boxes](https://download.libsodium.org/doc/public-key_cryptography/sealed_boxes) using the [tweetnacl-js](https://github.com/dchest/tweetnacl-js) and [blakejs](https://github.com/dcposch/blakejs) libraries.
+
+ ## Usage
+
+ ```javascript
+const nacl = require('tweetnacl')
+const sodium = require('tweetsodium')
+
+// generate public key to use for encryption and coresponding secret key to use
+// for decryption
+const keyPair = nacl.box.keyPair()
+
+// encrypts message string using public key
+function encrypt(message) {
+    const encoder = new TextEncoder()
+    const messageBytes = encoder.encode(message)
+
+    return sodium.seal(messageBytes, keyPair.publicKey)
+}
+
+// decrypts message using secret key
+function decrypt(ciphertext) {
+    const encoder = new TextEncoder()
+    const ciphertextBytes = encoder.encode(ciphertext)
+
+    return sodium.sealOpen(ciphertextBytes, keyPair.PublicKey, keyPair.secretKey)
+}
+```
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,63 @@
+const nacl = require('tweetnacl')
+const {blake2bInit, blake2bUpdate, blake2bFinal} = require('blakejs/blake2b')
+
+// Authenticated sealing only prepends the nonce to the ciphertext. Anonymous
+// sealing also prepends a random public key.
+const overhead = nacl.box.overheadLength + nacl.box.publicKeyLength
+
+// Generates a 24 byte nonce that is a blake2b digest of the ephemeral
+// public key and the reipient's public key.
+//
+// Returns a 24-byte Uint8Array
+//
+// Parameters:
+// - epk - ephemeral public key Uint8Array
+// - publicKey - recipient's public key Uint8Array
+function sealNonce(epk, publicKey) {
+    let hash = blake2bInit(nacl.box.nonceLength, false)
+
+    blake2bUpdate(hash, epk)
+    blake2bUpdate(hash, publicKey)
+    return blake2bFinal(hash)
+}
+
+// Encrypt a message for a recipient.
+//
+// Returns a Uint8Array whose length is 48 bytes greater than the message's.
+//
+// Parameters:
+// - message - message Uint8Array to encrypt.
+// - publicKey - recipient's public key Uint8Array.
+function seal(message, publicKey) {
+    const ekp = nacl.box.keyPair()
+
+    let out = new Uint8Array(message.length + overhead)
+    out.set(ekp.publicKey, 0)
+
+    const nonce = sealNonce(ekp.publicKey, publicKey)
+
+    const ct = nacl.box(message, nonce, publicKey, ekp.secretKey)
+    out.set(ct, nacl.box.publicKeyLength)
+
+    return out
+}
+
+// Decrypt the ciphertext message using the secret key.
+//
+// Returns a Uint8Array whose length is 48 bytes less than the ciphertext's.
+//
+// Parameters:
+// - ciphertext - encrypted message Uint8Array.
+// - secretKey - secret key Uint8Array.
+function sealOpen(ciphertext, publicKey, secretKey) {
+    const epk = ciphertext.slice(0, nacl.box.publicKeyLength)
+    const nonce = sealNonce(epk, publicKey)
+    ciphertext = ciphertext.slice(nacl.box.publicKeyLength)
+
+    return nacl.box.open(ciphertext, nonce, epk, secretKey)
+}
+
+module.exports = {
+    seal: seal,
+    sealOpen: sealOpen
+}
